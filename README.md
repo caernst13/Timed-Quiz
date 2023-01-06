9150279655c321c12588e3a07dec2ca8c3a28eaf
caernst13 <caleb_ernst@yahoo.com>
2023-01-05 19:21:26 -0500

    added comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Timed-Quiz
+
+This project was about creating a quiz that involes a timer element and a local storage element. by clicking one of the answers you either increase or decrease the time remaining. when either time runs out or all questions are answered you are shown your score and given the option to save yuour name and score. 
+
+Most of what I learned was about event listeners and how different classes can interact with each other as well as improving a lot at understanding how to control what is shown on the screan with appending or changing visibility of certain elements. of course always more to learn. For starter for sure some polishing can be done and a lot of improvments can be made to the high score aspect. I am really happy with how the question all load and work with each other.
+
+Here is a link to the website: https://caernst13.github.io/Timed-Quiz/
+
+![here is waht it looks like:](./assets/images/Screenshot%202023-01-05%20190947.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// initalizing elements from the html
 var startButton = document.getElementById("start-button");
 var timerEl = document.querySelector(".timer-count");
 var intro = document.querySelector(".home");
@@ -5,9 +6,10 @@ var flagEl = document.querySelector(".flag");
 var scoreEl = document.getElementById("score")
 var nameEl = document.getElementById("name")
 
+//hiding the score button
 scoreEl.style.visibility = 'hidden';
 
-
+// initalizing elements for questions and answers and giving them classes
 const question = document.createElement("div");
 question.classList.add("questions");
 const answer1 = document.createElement("button");
@@ -19,44 +21,55 @@ answer3.classList.add("answers",);
 const answer4 = document.createElement("button");
 answer4.classList.add("answers",);
 
+//elements for the results
 const resultEl = document.createElement('div');
 resultEl.classList.add("result")
 
+//constants for taking in scores 
 const userName = document.createElement('h1');
 const userScore = document.createElement('h4');
 
+//all questions stored in an array
 const questions = ['Q1: What is another term for zero when scoring tennis?', 'Q2: What is the fewest number of points needed to win a 12 point tie breaker?', 'Q3: What are the numbers used to keep track of the number of points a player has?', 'Q4: how manyy points does a player need to win by to win a game?', 'Q5: what is it called when tow players are even in points?'];
 
+//all answers also stored in an array
 const answers1 = ['Love', '13', '0, 1, 2, 3, 4', '1', 'tied'];
 const answers2 = ['Base', '12', '1, 2, 3, 4, game', '2', 'Draw'];
 const answers3 = ['Start', '7', 'love 10, 20, 30 game', '3', 'fetter'];
 const answers4 = ['Null', '9', '0, 15, 30, 40, game', '4', 'duce'];
 
+//random variables used later
 var i = 0;
 var correct = 0;
 var incorrect = 0;
 var timerCount = 60;
 var stop = false
 
+//start button
 startButton.addEventListener("click", startQuiz);
 
+//clean up to start the quiz
 function startQuiz() {
   startTimer();
   intro.remove();
   generateQuestion();
 }
 
+//clean up to show scores
 var highScore = function (event) {
   event.preventDefault();
   
+  //take in deta and stores them in local storage
   var userNames = nameEl.value.trim()
   var userScores = timerCount;
   localStorage.setItem("userNames", userNames)
   localStorage.setItem("userScores", userScores)
 
+  //calls display scores function
   displayScores();
 }
 
+//creates timer
 function startTimer() {
   timer = setInterval(function() {
     timerCount--;
@@ -71,8 +84,12 @@ function startTimer() {
     }
   }, 1000);
 }
+
+//moves on to next question 
 var nextQuestion = function (event) {
   event.preventDefault();
+
+  //checks if answer was correct and either adds or subtracts time
   if(event.target.className[8]){
     correct++
     resultEl.textContent = 'Correct!'
@@ -83,6 +100,7 @@ var nextQuestion = function (event) {
     timerCount = timerCount - 10;
   }
 
+  //clean up to move on to next question 
   switch (i) {
     case 0: answer1.classList.remove('right'); break;
     case 1: answer3.classList.remove('right'); break;
@@ -91,8 +109,10 @@ var nextQuestion = function (event) {
     default: answer4.classList.remove('right'); 
   }
 
+  //increases question count
   i++
 
+  //checks if all questions are over, calls game over if they are and goes to next question if not
   if (i<5) {
     generateQuestion()
   } else {
@@ -100,7 +120,10 @@ var nextQuestion = function (event) {
   }
 }
 
+//generates questions
 function generateQuestion() {
+  
+  //adds the right class to coresponding answer 
   switch (i) {
     case 0: answer1.classList.add('right'); break;
     case 1: answer3.classList.add('right'); break;
@@ -108,6 +131,8 @@ function generateQuestion() {
     case 3: answer2.classList.add('right'); break;
     default: answer4.classList.add('right'); 
   }
+
+  //adds text to questions and answers and appends them
   question.textContent = questions[i];
   flagEl.appendChild(question);
   answer1.textContent = answers1[i];
@@ -119,28 +144,37 @@ function generateQuestion() {
   answer4.textContent = answers4[i];
   question.appendChild(answer4);
 
+  //shows reuslt of the last question 
   flagEl.appendChild(resultEl)
 
+  //checks if a button was clicked and calls next question function 
   var subbmit = document.querySelector(".questions")
   subbmit.addEventListener("click", nextQuestion)
-
 }
 
+//function for end of game
 function gameOver () {
+  //clean up for showing results
   stop = true;
   question.remove();
   resultEl.remove();
+
+  //displays resutls 
   question.textContent = 'All Done!'
   const displayResults = document.createElement('h4');
   displayResults.textContent = 'Your score was: ' + timerCount + '!'
   flagEl.appendChild(question);
   question.appendChild(displayResults);
+
+  //shows the submit name for high score button 
   scoreEl.style.visibility = 'visible';
   timerEl.style.visibility = 'hidden';
   
+  //calls highscore 
   scoreEl.addEventListener('submit', highScore);
 }
 
+//displays scores 
 function displayScores () {
   question.remove();
   scoreEl.remove();
